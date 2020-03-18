8d8cce68dd567964235d7e72db06e59da08fdb21
Hyndavi Musipatla <hyndavi.musipatla1996@gmail.com>
2020-03-17 23:20:11 -0500

    First commit

---
+++ src/test/java/com/hyndavi/springboot/crud/shoppingapp/ShoppingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyndavi.springboot.crud.shoppingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
