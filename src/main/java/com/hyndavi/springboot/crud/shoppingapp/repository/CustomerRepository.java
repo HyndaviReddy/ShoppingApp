79de4ac9b632ada1d86182ec3e33d7d8d01ba2a5
Hyndavi Musipatla <hyndavi.musipatla1996@gmail.com>
2020-03-18 23:09:33 -0500

    Customer repository

--- src/main/java/com/hyndavi/springboot/crud/shoppingapp/repository/CustomerRepository.java
+++ src/main/java/com/hyndavi/springboot/crud/shoppingapp/repository/CustomerRepository.java
@@ -3,7 +3,6 @@ package com.hyndavi.springboot.crud.shoppingapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hyndavi.springboot.crud.shoppingapp.model.Customer;
-import com.hyndavi.springboot.crud.shoppingapp.model.OrderDetails;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
